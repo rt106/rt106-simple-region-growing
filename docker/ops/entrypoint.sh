fe1c6aea6fe5e2b83672b97a9ed62e444566f16e
Sarachan, Brion D <sarachan@ge.com>
2017-10-25 17:01:09 -0400

    Added notice file and copyright line to each source file.

--- docker/ops/entrypoint.sh
+++ docker/ops/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Copyright (c) General Electric Company, 2017.  All rights reserved.
 
 # start a server to serve the REST API to the algorithm container
 /usr/bin/python ./rt106GenericAdaptorREST.py & sleep 3
